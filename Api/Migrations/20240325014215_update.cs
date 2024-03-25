87d906eeb9dc88824bdfa3eb73478dde79061497
Sean Golden <sean.golden.one@gmail.com>
2024-03-25 01:48:02 +0000

    Db Update

---
+++ Api/Migrations/20240325014215_update.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TravelPlannerApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class update : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_City_Plan_PlanId",
+                table: "City");
+
+            migrationBuilder.DropIndex(
+                name: "IX_City_PlanId",
+                table: "City");
+
+            migrationBuilder.DropColumn(
+                name: "PlanId",
+                table: "City");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PlanId",
+                table: "City",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_City_PlanId",
+                table: "City",
+                column: "PlanId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_City_Plan_PlanId",
+                table: "City",
+                column: "PlanId",
+                principalTable: "Plan",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
