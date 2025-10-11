986525c51ad94662117eed0bba33b9a4c65fac04
Sean Golden <sean.golden.one@gmail.com>
2025-10-10 20:39:14 -0700

    Add Country Autocomplete to AI Gen Plan

--- website/src/app/plan/ai-gen-plan/ai-gen-plan.component.ts
+++ website/src/app/plan/ai-gen-plan/ai-gen-plan.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { Plan } from 'src/app/models/plan';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { DataService } from 'src/app/data.service';
@@ -12,16 +12,25 @@ import { AccountService } from 'src/app/account.service';
   styleUrls: ['./ai-gen-plan.component.css'],
 })
 export class AiGenPlanComponent {
+  @ViewChild('input') input: ElementRef<HTMLInputElement>;
   public country: string;
   countryList: string[] = [];
+  options: string[] = [];
+  filteredOptions: string[];
 
   constructor(private data: DataService, private toastr: ToastrService, private router: Router,private accountService:AccountService) {
-
+this.data.getRefCountryList().subscribe({
+      next: countries => {
+        this.options = countries.map(c => c.name);
+        this.filteredOptions = this.options.slice();
+      }
+    });
   }
 
-  addCountry() {
-    this.countryList.push(this.country);
+  addCountry(country:string) {
+    this.countryList.push(country);
     this.country = "";
+    this.input.nativeElement.value = '';
   }
 
   removeCountry(name: string) {
@@ -30,11 +39,22 @@ export class AiGenPlanComponent {
 
   }
 
+    filter(): void {
+    const filterValue = this.input.nativeElement.value.toLowerCase();
+    this.filteredOptions = this.options.filter(o => o.toLowerCase().includes(filterValue));
+  }
+
+      isInList(country: string): boolean {
+  return this.countryList.includes(country);
+}
+
   generatePlan() {
     if(this.countryList.length == 0){
       this.toastr.error("Please Add At Least One Country");
       return;
     }
+
+
 
     const plan: any = {};
     plan.countries = this.countryList;
